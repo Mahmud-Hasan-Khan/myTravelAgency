5aeec8022d075df6de1014e5c434aa6b334b0c7f
Mahmud Hasan <mahmud.hasan2006@gmail.com>
2025-04-25 23:22:54 +0600

    Google review added

---
+++ src/app/components/GoogleReviews/GoogleReviews.jsx
@@ -0,0 +1,24 @@
+"use client"
+import React, { useEffect } from 'react';
+
+const GoogleReviews = () => {
+    useEffect(() => {
+        const existingScript = document.querySelector(
+            'script[src="https://static.elfsight.com/platform/platform.js"]'
+        );
+        if (existingScript) return; // Prevent loading the script multiple times
+
+        const script = document.createElement("script");
+        script.src = "https://static.elfsight.com/platform/platform.js"; // ✅ CORRECTED
+        script.async = true;
+        document.body.appendChild(script);
+    }, []);
+
+    return (
+        <div>
+            <div className="elfsight-app-988aa58e-51ae-41c8-807a-d5a716d7faa2" data-elfsight-app-lazy></div>
+        </div>
+    );
+};
+
+export default GoogleReviews;
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import GoogleReviews from "./components/GoogleReviews/GoogleReviews";
 import SearchForm from "./components/HomePage/SearchForm/SearchForm";
 import VideoBanner from "./components/HomePage/VideoBanner/VideoBanner";
 
@@ -5,18 +6,14 @@ import VideoBanner from "./components/HomePage/VideoBanner/VideoBanner";
 export default function Home() {
   return (
     <div>
+
       <div className="relative">
         <VideoBanner></VideoBanner>
         <div className="absolute lg:top-48 left-1/2 transform -translate-x-1/2">
           <SearchForm></SearchForm>
+          <GoogleReviews></GoogleReviews>
         </div>
       </div>
-      {/* <div className="container px-4 md:px-32 py-6">
-        <Banner />
-      </div> */}
-
-
-
     </div>
   );
 }
