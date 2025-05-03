9cd04782cf2861077e7cd4f2314f574bc82c9cdb
Mahmud Hasan <mahmud.hasan2006@gmail.com>
2025-05-03 17:43:07 +0600

    Marquee animation smoother and more efficient

--- src/app/components/HomePage/AirlinesPartnerMarquee/AirlinesPartnerMarquee.jsx
+++ src/app/components/HomePage/AirlinesPartnerMarquee/AirlinesPartnerMarquee.jsx
@@ -3,37 +3,38 @@ import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 import Marquee from 'react-fast-marquee';
 import SectionTitle from '../../SectionTitle/SectionTitle';
+import airlinesData from '../../../../../public/airlinesPartnerLogo.json'
 
 const AirlinesPartnerMarquee = () => {
 
   // State to store airlinesLogo data
-  const [airlinesLogo, setAirlinesLogo] = useState([]);
+  const [airlinesLogo, setAirlinesLogo] = useState(airlinesData);
 
   // Fetch airlinesLogo data from local JSON file
-  useEffect(() => {
-    const fetchAirlinesLogo = async () => {
-      const response = await fetch('/airlinesPartnerLogo.json');
-      const data = await response.json();
-      console.log(data);
-      setAirlinesLogo(data);
-    };
-    fetchAirlinesLogo();
-  }, []);
+  // useEffect(() => {
+  //   const fetchAirlinesLogo = async () => {
+  //     const response = await fetch('/airlinesPartnerLogo.json');
+  //     const data = await response.json();
+  //     console.log(data);
+  //     setAirlinesLogo(data);
+  //   };
+  //   fetchAirlinesLogo();
+  // }, []);
 
   return (
     <div className="my-3">
-      
       <SectionTitle heading="Top Airlines Partner" subheading="Wakia Travels connects all top airlines. Enjoy a comfortable and hassle-free journey on any destination and get tickets of top airlines easily." />
       <div className="">
-        <Marquee pauseOnHover speed={80}>
+        <Marquee pauseOnHover speed={60} gradient={false}>
           {airlinesLogo.map((logo, index) => (
-            <div key={index} className='mx-6'>
+            <div key={index} className='mx-6 h-12 flex items-center justify-center'>
               <Image
                 src={logo.image}
                 alt={logo.name || 'Airline Logo'}
                 width={80}
                 height={40}
                 className="object-contain h-12 w-auto"
+                priority
               />
             </div>
           ))}
