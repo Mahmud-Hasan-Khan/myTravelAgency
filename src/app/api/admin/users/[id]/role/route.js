5311150d27d257721b18533c49ff258d89828bab
Mahmud Hasan <mahmud.hasan2006@gmail.com>
2025-06-05 20:26:22 +0600

    sideber issue fixed for Admin

--- src/app/api/admin/users/[id]/role/route.js
+++ src/app/api/admin/users/[id]/role/route.js
@@ -1,28 +1,3 @@
-// import dbConnect from '@/lib/dbConnect';
-// import { ObjectId } from 'mongodb';
-// import { NextResponse } from 'next/server';
-
-
-// export async function PUT(req, context) {
-//   const params = await context.params;
-//   const { id } = params;
-
-//   const { role } = await req.json();
-
-//   if (!['admin', 'user'].includes(role)) {
-//     return NextResponse.json({ error: 'Invalid role' }, { status: 400 });
-//   }
-
-//   try {
-//     const { collection } = await dbConnect('users');
-//     await collection.updateOne({ _id: new ObjectId(id) }, { $set: { role } });
-
-//   } catch (error) {
-//     console.error('Role Update Error:', error);
-//     return NextResponse.json({ error: 'Failed to update role' }, { status: 500 });
-//   }
-// }
-
 import dbConnect from '@/lib/dbConnect';
 import { ObjectId } from 'mongodb';
 import { NextResponse } from 'next/server';
